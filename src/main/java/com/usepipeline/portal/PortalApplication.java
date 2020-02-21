2e9e310940361ab375738533daddb43252ddc991
Gavin Killough <gavin.killough@usepipeline.com>
2020-02-21 14:09:30 +0000

    Feature: Create authentication schema

--- src/main/java/com/usepipeline/portal/PortalApplication.java
+++ src/main/java/com/usepipeline/portal/PortalApplication.java
@@ -2,13 +2,16 @@ package com.usepipeline.portal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-
+@EnableJpaRepositories(basePackages = {"com.usepipeline.database"})
+@EnableTransactionManagement
 @SpringBootApplication
 public class PortalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PortalApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PortalApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/usepipeline/portal/database/authentication/repository/LoginRepository.java
@@ -0,0 +1,9 @@
+package com.usepipeline.portal.database.authentication.repository;
+
+import com.usepipeline.portal.database.authentication.entity.LoginEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public interface LoginRepository extends JpaRepository<LoginEntity, Long> {
+}
