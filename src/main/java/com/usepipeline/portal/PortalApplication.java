d7ce1ac85e186926b3b4eae28204cab184043a33
Gavin Killough <gavin.killough@usepipeline.com>
2020-02-29 11:23:02 -0500

    Uncomment annotations now that they are needed

--- src/main/java/com/usepipeline/portal/PortalApplication.java
+++ src/main/java/com/usepipeline/portal/PortalApplication.java
@@ -2,13 +2,15 @@ package com.usepipeline.portal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement
-// @EnableGlobalMethodSecurity(securedEnabled = true)
+@EnableGlobalMethodSecurity(securedEnabled = true)
 @EnableJpaRepositories(basePackages = {"com.usepipeline.portal.database"})
-@SpringBootApplication // (exclude = {BatchAutoConfiguration.class})
+@SpringBootApplication(exclude = {BatchAutoConfiguration.class})
 public class PortalApplication {
 
     public static void main(String[] args) {
