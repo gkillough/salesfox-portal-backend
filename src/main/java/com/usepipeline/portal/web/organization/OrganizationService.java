dda7b754dd48bb48701387050106abdeb38fceaa
Gavin Killough <gavin.killough@usepipeline.com>
2020-04-07 21:35:59 -0400

    Add abstraction for http-safe database reads related to user memberships

--- src/main/java/com/usepipeline/portal/web/organization/OrganizationService.java
+++ src/main/java/com/usepipeline/portal/web/organization/OrganizationService.java
@@ -2,58 +2,29 @@ package com.usepipeline.portal.web.organization;
 
 import com.usepipeline.portal.database.account.entity.MembershipEntity;
 import com.usepipeline.portal.database.account.entity.UserEntity;
-import com.usepipeline.portal.database.account.repository.MembershipRepository;
-import com.usepipeline.portal.database.account.repository.UserRepository;
 import com.usepipeline.portal.database.organization.OrganizationEntity;
-import com.usepipeline.portal.database.organization.OrganizationRepository;
 import com.usepipeline.portal.database.organization.account.OrganizationAccountEntity;
-import com.usepipeline.portal.database.organization.account.OrganizationAccountRepository;
 import com.usepipeline.portal.web.organization.model.OrganizationAccountModel;
-import com.usepipeline.portal.web.security.authentication.SecurityContextUtils;
+import com.usepipeline.portal.web.util.HttpSafeUserMembershipRetrievalService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
-import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @Component
 public class OrganizationService {
-    private UserRepository userRepository;
-    private MembershipRepository membershipRepository;
-    private OrganizationRepository organizationRepository;
-    private OrganizationAccountRepository organizationAccountRepository;
+    private HttpSafeUserMembershipRetrievalService userMembershipRetrievalService;
 
     @Autowired
-    public OrganizationService(UserRepository userRepository, MembershipRepository membershipRepository, OrganizationRepository organizationRepository, OrganizationAccountRepository organizationAccountRepository) {
-        this.userRepository = userRepository;
-        this.membershipRepository = membershipRepository;
-        this.organizationRepository = organizationRepository;
-        this.organizationAccountRepository = organizationAccountRepository;
+    public OrganizationService(HttpSafeUserMembershipRetrievalService userMembershipRetrievalService) {
+        this.userMembershipRetrievalService = userMembershipRetrievalService;
     }
 
     public OrganizationAccountModel getOrganizationAccount() {
-        UserEntity loggedInUserEntity = SecurityContextUtils.retrieveUserAuthToken()
-                .map(SecurityContextUtils::extractUserDetails)
-                .map(UserDetails::getUsername)
-                .flatMap(userRepository::findFirstByEmail)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED));
-
-        // TODO abstract this repeated lookup logic into a service
-        OrganizationAccountEntity organizationAccountEntity = membershipRepository.findFirstByUserId(loggedInUserEntity.getUserId())
-                .map(MembershipEntity::getOrganizationAccountId)
-                .flatMap(organizationAccountRepository::findById)
-                .orElseThrow(() -> {
-                    log.error("Missing Organization Account for user '{}' with id {}", loggedInUserEntity.getEmail(), loggedInUserEntity.getUserId());
-                    return new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
-                });
-
-        OrganizationEntity organizationEntity = organizationRepository.findById(organizationAccountEntity.getOrganizationId())
-                .orElseThrow(() -> {
-                    log.error("Missing Organization for Organization Account '{}' with id {}", organizationAccountEntity.getOrganizationAccountName(), organizationAccountEntity.getOrganizationAccountId());
-                    return new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
-                });
+        UserEntity loggedInUserEntity = userMembershipRetrievalService.getAuthenticatedUserEntity();
+        MembershipEntity membershipEntity = userMembershipRetrievalService.getMembershipEntity(loggedInUserEntity);
+        OrganizationAccountEntity organizationAccountEntity = userMembershipRetrievalService.getOrganizationAccountEntity(membershipEntity);
+        OrganizationEntity organizationEntity = userMembershipRetrievalService.getOrganizationEntity(organizationAccountEntity);
 
         return new OrganizationAccountModel(organizationEntity.getOrganizationName(), organizationAccountEntity.getOrganizationAccountName());
     }
---
+++ src/main/java/com/usepipeline/portal/web/util/HttpSafeUserMembershipRetrievalService.java
@@ -0,0 +1,81 @@
+package com.usepipeline.portal.web.util;
+
+import com.usepipeline.portal.database.account.entity.MembershipEntity;
+import com.usepipeline.portal.database.account.entity.UserEntity;
+import com.usepipeline.portal.database.account.repository.MembershipRepository;
+import com.usepipeline.portal.database.account.repository.UserRepository;
+import com.usepipeline.portal.database.organization.OrganizationEntity;
+import com.usepipeline.portal.database.organization.OrganizationRepository;
+import com.usepipeline.portal.database.organization.account.OrganizationAccountEntity;
+import com.usepipeline.portal.database.organization.account.OrganizationAccountRepository;
+import com.usepipeline.portal.web.security.authentication.SecurityContextUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.constraints.NotNull;
+import java.util.Optional;
+
+@Slf4j
+@Component
+public class HttpSafeUserMembershipRetrievalService {
+    private UserRepository userRepository;
+    private MembershipRepository membershipRepository;
+    private OrganizationRepository organizationRepository;
+    private OrganizationAccountRepository organizationAccountRepository;
+
+    @Autowired
+    public HttpSafeUserMembershipRetrievalService(UserRepository userRepository, MembershipRepository membershipRepository,
+                                                  OrganizationRepository organizationRepository, OrganizationAccountRepository organizationAccountRepository) {
+        this.userRepository = userRepository;
+        this.membershipRepository = membershipRepository;
+        this.organizationRepository = organizationRepository;
+        this.organizationAccountRepository = organizationAccountRepository;
+    }
+
+    public UserEntity getAuthenticatedUserEntity() {
+        Optional<UsernamePasswordAuthenticationToken> authToken = SecurityContextUtils.retrieveUserAuthToken();
+        if (authToken.isPresent()) {
+            UserDetails userDetails = SecurityContextUtils.extractUserDetails(authToken.get());
+            return getExistingUserByEmail(userDetails.getUsername());
+        }
+        throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+    }
+
+    public UserEntity getExistingUserByEmail(@NotNull String email) {
+        return userRepository.findFirstByEmail(email)
+                .orElseThrow(() -> {
+                    log.error("Expected user with email [{}] to exist in the database", email);
+                    return new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+                });
+    }
+
+    public MembershipEntity getMembershipEntity(@NotNull UserEntity userEntity) {
+        return membershipRepository.findFirstByUserId(userEntity.getUserId())
+                .orElseThrow(() -> {
+                    log.error("Expected membership for user with id [{}] to exist in the database", userEntity.getUserId());
+                    return new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+                });
+    }
+
+    public OrganizationAccountEntity getOrganizationAccountEntity(@NotNull MembershipEntity membershipEntity) {
+        return organizationAccountRepository.findById(membershipEntity.getOrganizationAccountId())
+                .orElseThrow(() -> {
+                    log.error("Expected organization account for user with id [{}], with membership id [{}] to exist in the database", membershipEntity.getUserId(), membershipEntity.getMembershipId());
+                    return new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+                });
+    }
+
+    public OrganizationEntity getOrganizationEntity(@NotNull OrganizationAccountEntity organizationAccountEntity) {
+        return organizationRepository.findById(organizationAccountEntity.getOrganizationId())
+                .orElseThrow(() -> {
+                    log.error("Missing Organization for Organization Account '{}' with id {}", organizationAccountEntity.getOrganizationAccountName(), organizationAccountEntity.getOrganizationAccountId());
+                    return new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+                });
+    }
+
+}
