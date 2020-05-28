69a0366792eda67f40ba7b9c9e9239f4f7a45f8b
Gavin Killough <gavin.killough@usepipeline.com>
2020-05-27 21:25:25 -0400

    Add icon read and delete methods

--- src/main/java/com/usepipeline/portal/common/service/icon/LocalIconManager.java
+++ src/main/java/com/usepipeline/portal/common/service/icon/LocalIconManager.java
@@ -14,6 +14,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.Optional;
 
 @Component
 public class LocalIconManager {
@@ -49,7 +50,24 @@ public class LocalIconManager {
         }
     }
 
-    // TODO retrieveIcon
+    public Optional<File> retrieveIcon(String iconFileName) throws PortalFileSystemException {
+        File iconDir = retrieveIconDirAsFile();
+        File iconFile = new File(iconDir, iconFileName);
+        if (iconFile.exists()) {
+            return Optional.of(iconFile);
+        }
+        return Optional.empty();
+    }
+
+    public boolean deleteIcon(String iconFileName) throws PortalFileSystemException {
+        File iconDir = retrieveIconDirAsFile();
+
+        File iconFile = new File(iconDir, iconFileName);
+        if (iconFile.exists() && iconFile.canWrite()) {
+            return iconFile.delete();
+        }
+        return false;
+    }
 
     private File retrieveIconDirAsFile() throws PortalFileSystemException {
         String iconDirName = resourceDirConfig.getIconDir();
