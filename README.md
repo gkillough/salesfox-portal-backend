92df313c0343015ea190807692cf452d0a8e13aa
Gavin Killough <gavin.killough@getboostr.com>
2020-07-30 22:37:58 -0400

    Update README and test DB init script to grant correct permissions to portaluser

--- README.md
+++ README.md
@@ -12,3 +12,26 @@ The gradle `runServer` task has two flags that can be set:
 - `--reuseContainer` will keep the database from the previous run
 - `--suspend` will wait for a debug connection (on port 9095) as soon as the application server starts
 
+## Set up Postgres
+1. Create _portaladmin_ role: 
+```sql 
+CREATE ROLE portaladmin WITH SUPERUSER LOGIN ENCRYPTED PASSWORD 'Port@l!23'
+```
+
+2. Create database: 
+```sql
+CREATE DATABASE boostr WITH OWNER portaladmin;
+```
+
+3. Create schema:
+```sql
+CREATE SCHEMA portal;
+```
+
+4. Create _portaluser_ role: 
+```sql
+CREATE ROLE portaluser WITH LOGIN ENCRYPTED PASSWORD 'Port@l!23'
+
+GRANT USAGE ON SCHEMA portal TO portaluser;
+ALTER DEFAULT PRIVILEGES IN SCHEMA portal GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO portaluser;
+```
--- buildSrc/src/main/java/com/getboostr/portal/build/RunServerTask.java
+++ buildSrc/src/main/java/com/getboostr/portal/build/RunServerTask.java
@@ -62,43 +62,46 @@ public class RunServerTask extends Exec {
 
     public List<String> getDebugVariables() {
         return List.of(
-            "-Xdebug",
-            "-Xrunjdwp:transport=dt_socket,server=y,address=9095,suspend=" + (suspend ? "y" : "n")
+                "-Xdebug",
+                "-Xrunjdwp:transport=dt_socket,server=y,address=9095,suspend=" + (suspend ? "y" : "n")
         );
     }
 
     public List<String> getJMXVariables() {
         return List.of("-Dcom.sun.management.jmxremote",
-            "-Dcom.sun.management.jmxremote.port=9045",
-            "-Dcom.sun.management.jmxremote.local.only=false",
-            "-Dcom.sun.management.jmxremote.authenticate=false",
-            "-Dcom.sun.management.jmxremote.ssl=false"
+                "-Dcom.sun.management.jmxremote.port=9045",
+                "-Dcom.sun.management.jmxremote.local.only=false",
+                "-Dcom.sun.management.jmxremote.authenticate=false",
+                "-Dcom.sun.management.jmxremote.ssl=false"
         );
     }
 
     public List<String> getApplicationVariables() {
         return List.of(
-            "--server.port=8080",
-            "--spring.jpa.hibernate.ddl-auto=none",
-            "--spring.jpa.database-platform=org.hibernate.dialect.PostgreSQLDialect",
+                "--server.port=8080",
+                "--spring.jpa.hibernate.ddl-auto=none",
+                "--spring.jpa.database-platform=org.hibernate.dialect.PostgreSQLDialect",
 
-            // https://github.com/testcontainers/testcontainers-spring-boot
-            "--embedded.postgresql.enabled=true",
-            "--embedded.postgresql.dockerImage=postgres:" + postgresVersion,
-            "--embedded.postgresql.reuseContainer=" + reuseContainer,
-            "--embedded.postgresql.waitTimeoutInSeconds=20",
-            "--embedded.containers.forceShutdown=true",
+                // https://github.com/testcontainers/testcontainers-spring-boot
+                "--embedded.postgresql.enabled=true",
+                "--embedded.postgresql.dockerImage=postgres:" + postgresVersion,
+                "--embedded.postgresql.reuseContainer=" + reuseContainer,
+                "--embedded.postgresql.waitTimeoutInSeconds=20",
+                "--embedded.containers.forceShutdown=true",
 
-            "--embedded.postgresql.schema=portal",
-            "--embedded.postgresql.user=root",
-            "--embedded.postgresql.password=root",
-            "--embedded.postgresql.initScriptPath=file:src/test/resources/testDatabase/init_test_db.sql",
+                "--embedded.postgresql.schema=portal",
+                "--embedded.postgresql.user=root",
+                "--embedded.postgresql.password=root",
+                "--embedded.postgresql.initScriptPath=file:buildSrc/src/main/resources/init_test_db.sql",
 
-            "--hibernate.default_schema=${embedded.postgresql.schema=portal}",
-            "--spring.datasource.username=${embedded.postgresql.user}",
-            "--spring.datasource.password=${embedded.postgresql.password}",
-            "--spring.datasource.url=jdbc:postgresql://${embedded.postgresql.host}:${embedded.postgresql.port}/boostr",
-            "--spring.datasource.hikari.jdbc-url=jdbc:postgresql://${embedded.postgresql.host}:${embedded.postgresql.port}/boostr"
+                "--spring.liquibase.user=portaladmin",
+                "--spring.liquibase.password=Port@l!23",
+
+                "--hibernate.default_schema=portal",
+                "--spring.datasource.username=portaluser",
+                "--spring.datasource.password=Port@l!23",
+                "--spring.datasource.url=jdbc:postgresql://${embedded.postgresql.host}:${embedded.postgresql.port}/boostr",
+                "--spring.datasource.hikari.jdbc-url=jdbc:postgresql://${embedded.postgresql.host}:${embedded.postgresql.port}/boostr"
         );
     }
 
