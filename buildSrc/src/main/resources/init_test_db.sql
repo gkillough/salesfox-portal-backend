ba46221c47f6e7730ffc1c8686ade3fd459024fb
Gavin Killough <gavin.killough@getboostr.com>
2020-08-01 14:11:27 -0400

    Add portaluser db role after schema creation in liquibase

--- buildSrc/src/main/resources/init_test_db.sql
+++ buildSrc/src/main/resources/init_test_db.sql
@@ -5,9 +5,9 @@ BEGIN
     -- create role and database if there are errors they will be logged.
     PERFORM dblink('user=root dbname=postgres', 'CREATE ROLE portaladmin WITH SUPERUSER LOGIN ENCRYPTED PASSWORD ''Port@l!23'' ', FALSE);
     PERFORM dblink('user=portaladmin dbname=postgres', 'CREATE DATABASE boostr WITH OWNER portaladmin', FALSE);
-    PERFORM dblink('user=portaladmin dbname=boostr', 'CREATE SCHEMA portal', FALSE);
-    PERFORM dblink('user=portaladmin dbname=boostr', 'CREATE ROLE portaluser WITH LOGIN PASSWORD ''Port@l!23'' ', FALSE);
-    PERFORM dblink('user=portaladmin dbname=boostr', 'GRANT USAGE ON SCHEMA portal TO portaluser', FALSE);
-    PERFORM dblink('user=portaladmin dbname=boostr', 'ALTER DEFAULT PRIVILEGES IN SCHEMA portal GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO portaluser', FALSE);
+    --PERFORM dblink('user=portaladmin dbname=boostr', 'CREATE SCHEMA portal', FALSE);
+    --PERFORM dblink('user=portaladmin dbname=boostr', 'CREATE ROLE portaluser WITH LOGIN PASSWORD ''Port@l!23'' ', FALSE);
+    --PERFORM dblink('user=portaladmin dbname=boostr', 'GRANT USAGE ON SCHEMA portal TO portaluser', FALSE);
+    --PERFORM dblink('user=portaladmin dbname=boostr', 'ALTER DEFAULT PRIVILEGES IN SCHEMA portal GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO portaluser', FALSE);
 END
 $$;
