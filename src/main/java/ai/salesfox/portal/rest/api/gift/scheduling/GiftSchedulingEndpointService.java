cf7076ac0d2135de00e2451ac4fa1253d3c72f08
Gavin Killough <gavin.killough@getboostr.com>
2020-08-26 19:34:23 -0400

    PORTALBE-37: Add validation to ensure scheduled date is in the future

--- src/main/java/ai/salesfox/portal/rest/api/gift/scheduling/GiftSchedulingEndpointService.java
+++ src/main/java/ai/salesfox/portal/rest/api/gift/scheduling/GiftSchedulingEndpointService.java
@@ -2,7 +2,9 @@ package ai.salesfox.portal.rest.api.gift.scheduling;
 
 import ai.salesfox.portal.common.FieldValidationUtils;
 import ai.salesfox.portal.common.enumeration.AccessOperation;
+import ai.salesfox.portal.common.enumeration.GiftTrackingStatus;
 import ai.salesfox.portal.common.model.PortalDateModel;
+import ai.salesfox.portal.common.service.gift.GiftTrackingService;
 import ai.salesfox.portal.database.account.entity.UserEntity;
 import ai.salesfox.portal.database.gift.GiftEntity;
 import ai.salesfox.portal.database.gift.GiftRepository;
@@ -13,8 +15,10 @@ import ai.salesfox.portal.rest.util.HttpSafeUserMembershipRetrievalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Component
@@ -22,16 +26,19 @@ public class GiftSchedulingEndpointService {
     private final GiftRepository giftRepository;
     private final GiftScheduleRepository giftScheduleRepository;
     private final GiftAccessService giftAccessService;
+    private final GiftTrackingService giftTrackingService;
     private final HttpSafeUserMembershipRetrievalService membershipRetrievalService;
 
     @Autowired
-    public GiftSchedulingEndpointService(GiftRepository giftRepository, GiftScheduleRepository giftScheduleRepository, GiftAccessService giftAccessService, HttpSafeUserMembershipRetrievalService membershipRetrievalService) {
+    public GiftSchedulingEndpointService(GiftRepository giftRepository, GiftScheduleRepository giftScheduleRepository, GiftAccessService giftAccessService, GiftTrackingService giftTrackingService, HttpSafeUserMembershipRetrievalService membershipRetrievalService) {
         this.giftRepository = giftRepository;
         this.giftScheduleRepository = giftScheduleRepository;
         this.giftAccessService = giftAccessService;
+        this.giftTrackingService = giftTrackingService;
         this.membershipRetrievalService = membershipRetrievalService;
     }
 
+    @Transactional
     public GiftScheduleResponseModel scheduleGiftDraftSubmission(UUID giftId, GiftScheduleRequestModel requestModel) {
         UserEntity loggedInUser = membershipRetrievalService.getAuthenticatedUserEntity();
         GiftEntity foundGift = findGiftAndValidateAccess(giftId, loggedInUser);
@@ -42,9 +49,12 @@ public class GiftSchedulingEndpointService {
 
         GiftScheduleEntity giftScheduleToSave = new GiftScheduleEntity(giftId, requestModel.getSendDate().toLocalDate(), loggedInUser.getUserId());
         GiftScheduleEntity savedGiftSchedule = giftScheduleRepository.save(giftScheduleToSave);
+
+        giftTrackingService.updateGiftTrackingInfo(foundGift, loggedInUser, GiftTrackingStatus.SCHEDULED);
         return new GiftScheduleResponseModel(savedGiftSchedule.getGiftId(), savedGiftSchedule.getSendDate());
     }
 
+    @Transactional
     public void updateGiftSchedule(UUID giftId, GiftScheduleRequestModel requestModel) {
         UserEntity loggedInUser = membershipRetrievalService.getAuthenticatedUserEntity();
         GiftEntity foundGift = findGiftAndValidateAccess(giftId, loggedInUser);
@@ -59,6 +69,7 @@ public class GiftSchedulingEndpointService {
         giftScheduleRepository.save(foundSchedule);
     }
 
+    @Transactional
     public void unscheduleGift(UUID giftId) {
         UserEntity loggedInUser = membershipRetrievalService.getAuthenticatedUserEntity();
         GiftEntity foundGift = findGiftAndValidateAccess(giftId, loggedInUser);
@@ -66,6 +77,7 @@ public class GiftSchedulingEndpointService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, String.format("Cannot unschedule a gift with the current status: %s", foundGift.getGiftTrackingEntity().getStatus()));
         }
         giftScheduleRepository.deleteById(giftId);
+        giftTrackingService.updateGiftTrackingInfo(foundGift, loggedInUser, GiftTrackingStatus.DRAFT);
     }
 
     private GiftEntity findGiftAndValidateAccess(UUID giftId, UserEntity loggedInUser) {
@@ -81,6 +93,8 @@ public class GiftSchedulingEndpointService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The send date is required");
         } else if (!FieldValidationUtils.isValidDate(sendDate)) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The send date is invalid");
+        } else if (!sendDate.toLocalDate().isAfter(LocalDate.now())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The send date must be in the future");
         }
     }
 
