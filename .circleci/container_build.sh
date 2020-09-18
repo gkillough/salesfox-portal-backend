cfe78afbdf40dea6ab38313991b32239001f3790
Dave Sudia <dsudia@gospotcheck.com>
2020-09-18 15:46:52 -0600

    publish the image before scanning

--- .circleci/container_build.sh
+++ .circleci/container_build.sh
@@ -7,9 +7,12 @@ SHORT_SHA=${BITBUCKET_COMMIT:0:7}
 DOCKER_IMAGE_NAME=${BITBUCKET_BRANCH}-${SHORT_SHA}
 repository="salesfox/portal-backend"
 
+docker login -u salesfox -p ${DOCKERHUB_PASSWORD}
+
 pack build -v $repository:$docker_image_name \
   --builder heroku/buildpacks:18 \
   --buildpack heroku/java \
   --buildpack heroku/gradle \
   --buildpack heroku/procfile \
-  --path .
+  --path . \
+  --publish
