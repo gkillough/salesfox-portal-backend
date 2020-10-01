9f25469f4ef494587df05e734f944742fbec3132
Gavin Killough <gavin.killough@getboostr.com>
2020-09-30 22:52:48 -0400

    PORTALBE-99: Change note size field to Small/Medium/Large rather than storing a decimal pt value

--- src/main/java/ai/salesfox/portal/common/service/note/NoteValidationUtils.java
+++ src/main/java/ai/salesfox/portal/common/service/note/NoteValidationUtils.java
@@ -2,24 +2,25 @@ package ai.salesfox.portal.common.service.note;
 
 import ai.salesfox.integration.common.util.SalesfoxEnumUtils;
 import ai.salesfox.integration.scribeless.enumeration.ScribelessHandwritingColors;
+import ai.salesfox.integration.scribeless.enumeration.ScribelessHandwritingSize;
 import ai.salesfox.integration.scribeless.enumeration.ScribelessHandwritingStyles;
 
 import java.util.Arrays;
 
 public class NoteValidationUtils {
     public static final int MAX_MESSAGE_CHARS = 2500;
-    public static final int MIN_ALLOWED_FONT_SIZE = 8;
-    public static final int MAX_ALLOWED_FONT_SIZE = 24;
 
+    // TODO add ALLOWED_<THINGS>_STRING constants
     public static final String[] ALLOWED_COLORS = SalesfoxEnumUtils.lowercaseValues(ScribelessHandwritingColors.values());
+    public static final String[] ALLOWED_FONT_SIZES = SalesfoxEnumUtils.capitalizeValues(ScribelessHandwritingSize.values());
     public static final String[] ALLOWED_HANDWRITING_STYLES = SalesfoxEnumUtils.lowercaseValues(ScribelessHandwritingStyles.values());
 
     public static boolean isValidMessageSize(String message) {
         return message.length() <= MAX_MESSAGE_CHARS;
     }
 
-    public static boolean isValidFontSize(Integer fontSize) {
-        return MIN_ALLOWED_FONT_SIZE <= fontSize && fontSize <= MAX_ALLOWED_FONT_SIZE;
+    public static boolean isValidFontSize(String fontSize) {
+        return anyMatch(ALLOWED_FONT_SIZES, fontSize);
     }
 
     public static boolean isValidFontColor(String color) {
--- src/main/java/ai/salesfox/portal/rest/api/note/model/NoteResponseModel.java
+++ src/main/java/ai/salesfox/portal/rest/api/note/model/NoteResponseModel.java
@@ -15,7 +15,7 @@ import java.util.UUID;
 public class NoteResponseModel {
     private UUID noteId;
     private String message;
-    private Integer fontSize;
+    private String fontSize;
     private String fontColor;
     private String handwritingStyle;
     private OffsetDateTime dateModified;
