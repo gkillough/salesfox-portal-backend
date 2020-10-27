4639b64cf740e9f8113bfa96c8b8220b0d3b06a6
Gavin Killough <gavin.killough@getboostr.com>
2020-10-26 22:19:53 -0400

    PORTALBE-116: Expand EmailMessagingService in anticipation of NOMS integration file attachments

--- src/main/java/ai/salesfox/portal/common/service/email/EmailMessagingService.java
+++ src/main/java/ai/salesfox/portal/common/service/email/EmailMessagingService.java
@@ -11,12 +11,16 @@ import javax.activation.DataSource;
 import javax.activation.FileDataSource;
 import javax.mail.*;
 import javax.mail.internet.*;
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Component
 public class EmailMessagingService {
+    public static final String ATTACHMENT_HEADER_CONTENT_ID = "Content-ID";
     private final EmailConfiguration emailConfiguration;
     private final EmailHtmlMessageCreator emailHtmlMessageCreator;
 
@@ -27,11 +31,15 @@ public class EmailMessagingService {
     }
 
     public void sendMessage(EmailMessageModel emailMessageModel) throws PortalEmailException {
+        sendMessage(emailMessageModel, List.of());
+    }
+
+    public void sendMessage(EmailMessageModel emailMessageModel, Collection<File> attachments) throws PortalEmailException {
         validateEmailConfiguration();
         validateEmailMessageModel(emailMessageModel);
         try {
             Session smtpMailServerSession = Session.getDefaultInstance(emailConfiguration.getSmtpProperties());
-            MimeMessage mimeMessage = createMimeMessage(smtpMailServerSession, emailMessageModel);
+            MimeMessage mimeMessage = createMimeMessage(smtpMailServerSession, emailMessageModel, attachments);
             sendMessage(smtpMailServerSession, mimeMessage);
         } catch (MessagingException e) {
             log.error("There was an issue sending the email message with the subject line [{}] to  the following recipients: {}", emailMessageModel.getSubjectLine(), emailMessageModel.getRecipients(), e);
@@ -60,7 +68,7 @@ public class EmailMessagingService {
         }
     }
 
-    private MimeMessage createMimeMessage(Session session, EmailMessageModel emailMessageModel) throws PortalEmailException, MessagingException {
+    private MimeMessage createMimeMessage(Session session, EmailMessageModel emailMessageModel, Collection<File> attachments) throws PortalEmailException, MessagingException {
         MimeMessage mimeMessage = new MimeMessage(session);
         mimeMessage.setSubject(emailMessageModel.getSubjectLine());
         mimeMessage.setFrom(emailConfiguration.getSmtpFrom());
@@ -71,18 +79,21 @@ public class EmailMessagingService {
         MimeMultipart mimeMultipart = new MimeMultipart();
 
         // Message Body
-        String htmlMessage = emailHtmlMessageCreator.createHtmlMessage(EmailHtmlMessageCreator.DEFAULT_EMAIL_TEMPLATE_NAME, emailMessageModel);
+        String templateFile = Optional.ofNullable(emailMessageModel.getTemplateFileName()).orElse(EmailHtmlMessageCreator.DEFAULT_EMAIL_TEMPLATE_NAME);
+        String htmlMessage = emailHtmlMessageCreator.createHtmlMessage(templateFile, emailMessageModel);
         BodyPart htmlBodyPart = new MimeBodyPart();
         htmlBodyPart.setContent(htmlMessage, "text/html");
         mimeMultipart.addBodyPart(htmlBodyPart);
 
         // Image attachment
-        BodyPart imageBodyPart = new MimeBodyPart();
-        DataSource imageDataSource = new FileDataSource(emailConfiguration.getLogoPng());
-        DataHandler imageDataHandler = new DataHandler(imageDataSource);
-        imageBodyPart.setDataHandler(imageDataHandler);
-        imageBodyPart.setHeader("Content-ID", "<logoImage>");
+        BodyPart imageBodyPart = createAttachmentBodyPart(emailConfiguration.getLogoPng());
+        imageBodyPart.setHeader(ATTACHMENT_HEADER_CONTENT_ID, "<logoImage>");
         mimeMultipart.addBodyPart(imageBodyPart);
+
+        for (File attachment : attachments) {
+            BodyPart attachmentBodyPart = createAttachmentBodyPart(attachment);
+            mimeMultipart.addBodyPart(attachmentBodyPart);
+        }
 
         mimeMessage.setContent(mimeMultipart);
         return mimeMessage;
@@ -110,6 +121,14 @@ public class EmailMessagingService {
 
         Address[] addressesArray = new Address[recipientEmailAddresses.size()];
         return recipientEmailAddresses.toArray(addressesArray);
+    }
+
+    private BodyPart createAttachmentBodyPart(File attachmentSource) throws MessagingException {
+        BodyPart attachmentBodyPart = new MimeBodyPart();
+        DataSource dataSource = new FileDataSource(attachmentSource);
+        DataHandler dataHandler = new DataHandler(dataSource);
+        attachmentBodyPart.setDataHandler(dataHandler);
+        return attachmentBodyPart;
     }
 
     private void sendMessage(Session session, MimeMessage mimeMessage) throws MessagingException {
--- src/main/java/ai/salesfox/portal/common/service/email/model/EmailMessageModel.java
+++ src/main/java/ai/salesfox/portal/common/service/email/model/EmailMessageModel.java
@@ -1,6 +1,6 @@
 package ai.salesfox.portal.common.service.email.model;
 
-import lombok.AllArgsConstructor;
+import ai.salesfox.portal.common.service.email.EmailHtmlMessageCreator;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,11 +8,18 @@ import java.util.List;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class EmailMessageModel {
     private List<String> recipients;
     private String subjectLine;
     private String messageTitle;
     private String primaryMessage;
+    private String templateFileName = EmailHtmlMessageCreator.DEFAULT_EMAIL_TEMPLATE_NAME;
+
+    public EmailMessageModel(List<String> recipients, String subjectLine, String messageTitle, String primaryMessage) {
+        this.recipients = recipients;
+        this.subjectLine = subjectLine;
+        this.messageTitle = messageTitle;
+        this.primaryMessage = primaryMessage;
+    }
 
 }
