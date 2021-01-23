7864166622b2183d9c4c0daa9108ade411a12782
Gavin Killough <gavin.killough@getboostr.com>
2021-01-23 16:51:10 -0500

    PORTALBE-161: Complete basic CRUD operations for Stripe products

--- src/main/java/ai/salesfox/portal/integration/stripe/StripeProductService.java
+++ src/main/java/ai/salesfox/portal/integration/stripe/StripeProductService.java
@@ -6,7 +6,9 @@ import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.Product;
 import com.stripe.model.ProductCollection;
+import com.stripe.param.ProductCreateParams;
 import com.stripe.param.ProductListParams;
+import com.stripe.param.ProductUpdateParams;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -20,7 +22,7 @@ public class StripeProductService {
     public ProductCollection requestProducts(boolean active, ProductListParams.Type productType) throws PortalException {
         Stripe.apiKey = stripeConfiguration.getStripeSecretKey();
         ProductListParams params = ProductListParams.builder()
-                .setActive(true)
+                .setActive(active)
                 .setType(productType)
                 .build();
         try {
@@ -39,13 +41,35 @@ public class StripeProductService {
         }
     }
 
-    public void deleteProductById(String productId, boolean deleteAssociatedPrices) throws PortalException {
+    public void createProduct(ProductCreateParams productCreateParams) throws PortalException {
         Stripe.apiKey = stripeConfiguration.getStripeSecretKey();
         try {
-            Product product = Product.retrieve(productId);
-            if (deleteAssociatedPrices) {
-                // TODO call StripePriceService to delete this product's prices (if any exist)
-            }
+            Product.create(productCreateParams);
+        } catch (StripeException e) {
+            throw new PortalException("Failed to create Product in Stripe", e);
+        }
+    }
+
+    public void updateProductById(String productId, ProductUpdateParams productUpdateParams) throws PortalException {
+        Stripe.apiKey = stripeConfiguration.getStripeSecretKey();
+        Product product = requestProductById(productId);
+
+        try {
+            product.update(productUpdateParams);
+        } catch (StripeException e) {
+            throw new PortalException("Failed to update Product in Stripe", e);
+        }
+    }
+
+    public void deleteProductById(String productId, boolean deleteAssociatedPrices) throws PortalException {
+        Stripe.apiKey = stripeConfiguration.getStripeSecretKey();
+        Product product = requestProductById(productId);
+
+        if (deleteAssociatedPrices) {
+            // TODO call StripePriceService to delete this product's prices (if any exist)
+        }
+
+        try {
             product.delete();
         } catch (StripeException e) {
             throw new PortalException("Failed to delete Product from Stripe", e);
