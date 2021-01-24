386feb1acde689f235c483a2ade9f81cb73e934c
Gavin Killough <gavin.killough@getboostr.com>
2021-01-24 11:40:48 -0500

    PORTALBE-161: Create Stripe price service

---
+++ src/main/java/ai/salesfox/portal/integration/stripe/StripePriceService.java
@@ -0,0 +1,67 @@
+package ai.salesfox.portal.integration.stripe;
+
+import ai.salesfox.portal.common.exception.PortalException;
+import ai.salesfox.portal.integration.stripe.configuration.StripeConfiguration;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Price;
+import com.stripe.model.PriceCollection;
+import com.stripe.param.PriceCreateParams;
+import com.stripe.param.PriceListParams;
+import com.stripe.param.PriceUpdateParams;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StripePriceService {
+    private final StripeConfiguration stripeConfiguration;
+
+    @Autowired
+    public StripePriceService(StripeConfiguration stripeConfiguration) {
+        this.stripeConfiguration = stripeConfiguration;
+    }
+
+    public PriceCollection retrievePrices(String productId, boolean active, PriceListParams.Type type) throws PortalException {
+        Stripe.apiKey = stripeConfiguration.getStripeSecretKey();
+        PriceListParams params = PriceListParams.builder()
+                .setProduct(productId)
+                .setActive(active)
+                .setType(type)
+                .build();
+
+        try {
+            return Price.list(params);
+        } catch (StripeException e) {
+            throw new PortalException("Failed to retrieve Prices for the specified Product from Stripe", e);
+        }
+    }
+
+    public Price retrievePriceById(String priceId) throws PortalException {
+        Stripe.apiKey = stripeConfiguration.getStripeSecretKey();
+        try {
+            return Price.retrieve(priceId);
+        } catch (StripeException e) {
+            throw new PortalException("Failed to retrieve Price from Stripe", e);
+        }
+    }
+
+    public Price createPrice(PriceCreateParams priceCreateParams) throws PortalException {
+        Stripe.apiKey = stripeConfiguration.getStripeSecretKey();
+        try {
+            return Price.create(priceCreateParams);
+        } catch (StripeException e) {
+            throw new PortalException("Failed to create Price in Stripe", e);
+        }
+    }
+
+    public void updatePriceById(String priceId, PriceUpdateParams priceUpdateParams) throws PortalException {
+        Stripe.apiKey = stripeConfiguration.getStripeSecretKey();
+        Price price = retrievePriceById(priceId);
+        try {
+            price.update(priceUpdateParams);
+        } catch (StripeException e) {
+            throw new PortalException("Failed to update Price in Stripe", e);
+        }
+    }
+
+}
--- src/main/java/ai/salesfox/portal/integration/stripe/StripeProductService.java
+++ src/main/java/ai/salesfox/portal/integration/stripe/StripeProductService.java
@@ -9,12 +9,14 @@ import com.stripe.model.ProductCollection;
 import com.stripe.param.ProductCreateParams;
 import com.stripe.param.ProductListParams;
 import com.stripe.param.ProductUpdateParams;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class StripeProductService {
     private final StripeConfiguration stripeConfiguration;
 
+    @Autowired
     public StripeProductService(StripeConfiguration stripeConfiguration) {
         this.stripeConfiguration = stripeConfiguration;
     }
@@ -41,10 +43,10 @@ public class StripeProductService {
         }
     }
 
-    public void createProduct(ProductCreateParams productCreateParams) throws PortalException {
+    public Product createProduct(ProductCreateParams productCreateParams) throws PortalException {
         Stripe.apiKey = stripeConfiguration.getStripeSecretKey();
         try {
-            Product.create(productCreateParams);
+            return Product.create(productCreateParams);
         } catch (StripeException e) {
             throw new PortalException("Failed to create Product in Stripe", e);
         }
@@ -58,21 +60,6 @@ public class StripeProductService {
             product.update(productUpdateParams);
         } catch (StripeException e) {
             throw new PortalException("Failed to update Product in Stripe", e);
-        }
-    }
-
-    public void deleteProductById(String productId, boolean deleteAssociatedPrices) throws PortalException {
-        Stripe.apiKey = stripeConfiguration.getStripeSecretKey();
-        Product product = requestProductById(productId);
-
-        if (deleteAssociatedPrices) {
-            // TODO call StripePriceService to delete this product's prices (if any exist)
-        }
-
-        try {
-            product.delete();
-        } catch (StripeException e) {
-            throw new PortalException("Failed to delete Product from Stripe", e);
         }
     }
 
